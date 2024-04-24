d7ef9c453a6e5da3b6a0b5d9a3a5bbcfb61bcf24
taras_shevch <taras.shevchuk@gmail.com>
2024-04-24 20:02:07 +0200

    hide job postings which match preconfigured patterns.

--- jobsuche-chrome-extension/js/content.js
+++ jobsuche-chrome-extension/js/content.js
@@ -1,3 +1,4 @@
+
 setInterval(() => {
     let xingPostingSaved = document.querySelectorAll('article:has(button[data-testid="bookmark-action-button-filled"])')
 
@@ -6,3 +7,26 @@ setInterval(() => {
       });    
 }, 3000);
 
+
+let jobTitlePatterns = [
+  /Architect/g,
+  /^Senior\s/g,
+  /^Sr\.\s/g,
+];
+
+
+setInterval(() => {
+  const xingPostingSaved = document.querySelectorAll('article')
+
+  xingPostingSaved.forEach((item) => {
+      const jobPostingTitle = item.querySelector('h3[data-cy="job-teaser-list-title"]').textContent
+
+      jobTitlePatterns.forEach(
+        (pattern) => {
+          if (jobPostingTitle.match(pattern)) {
+            item.style.display = "none";
+          }
+        }
+      ); 
+  });    
+}, 3000);
