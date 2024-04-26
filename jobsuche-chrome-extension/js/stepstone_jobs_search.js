d5fda4d18a2363085d54b60f03fdb3d0673d7a2b
taras_shevch <taras.shevchuk@gmail.com>
2024-04-26 18:39:53 +0200

    update job title filters

--- jobsuche-chrome-extension/js/stepstone_jobs_search.js
+++ jobsuche-chrome-extension/js/stepstone_jobs_search.js
@@ -2,14 +2,29 @@
 console.log('stepstone_jobs_search.js')
 
 let jobTitlePatterns = [
+    /\bAdministrator\b/g,
+    /\bAllrounder\b/g,
+    /\bApplikationsbetreuer\b/g,
     /\bArchitect\b/g,
     /\bArchitekt\b/g,
+    /\bBerater\b/g,
+    /\bCoordinator\b/g,
+    /\bDevOps\b/g,
+    /\bLead\b/g,
+    /\bProjektmanager\b/g,
+    /\bProzessberater\b/g,
     /\bSoftwarearchitekt\b/g,
+    /\bSoftwaretester\b/g,
+    /\bSystemadministrator\b/g,
     /\bSystemarchitekt\b/g,
+    /\bTestautomatisierer\b/g,
+    /\bTestautomatisierung\b/g,
+    /\bTester\b/g,
+    /\bTestmanager\b/g,
+    /\bWerkstudent\b/g,
+    /\Consultant\b/g,
     /^Senior\s/g,
-    /^Sr\.\s/g,
-    /\bAdministrator\b/g,
-    /\bSystemadministrator\b/g
+    /^Sr\.\s/g
 ];
 
 
