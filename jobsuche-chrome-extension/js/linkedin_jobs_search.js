e311cbf61e5845399bae65be52d1d4aa26c107e8
taras_shevch <taras.shevchuk@gmail.com>
2024-05-02 18:53:31 +0200

    linkedin - fix job search filter by job title

--- jobsuche-chrome-extension/js/linkedin_jobs_search.js
+++ jobsuche-chrome-extension/js/linkedin_jobs_search.js
@@ -1,31 +1,23 @@
 console.log('linkedin_jobs_search.js')
 
-function hideJobsByJobTitle() {
-    (() => {
-        const xingPostingSaved = document.querySelectorAll('li.jobs-search-results__list-item')
-    
-        xingPostingSaved.forEach((item) => {
-            const jobPostingTitle = item.querySelector('a.job-card-list__title strong')?.textContent
+setInterval(() => {
+    const xingPostingSaved = document.querySelectorAll('li.jobs-search-results__list-item')
 
-            if (!jobPostingTitle) {
-                return
+    xingPostingSaved.forEach((item) => {
+        const jobPostingTitle = item.querySelector('a.job-card-list__title strong')?.textContent
+
+        if (!jobPostingTitle) {
+            return
+        }
+
+        console.log(jobPostingTitle)
+
+        for (const index in jobsucheJobTitlePatterns) {
+            if (jobPostingTitle.match(jobsucheJobTitlePatterns[index])) {
+                item.style.display = "none";
+                break;
             }
+        }
 
-            console.log(jobPostingTitle)
-    
-            for (const index in jobsucheJobTitlePatterns) {
-                if (jobPostingTitle.match(jobsucheJobTitlePatterns[index])) {
-                    item.style.display = "blank";
-                    break;
-                }
-            }
-    
-        });
-    }, 3000);
-}
-
-hideJobsByJobTitle();
-
-navigation.addEventListener('navigate', (event) => {
-    hideJobsByJobTitle();
-});
+    });
+}, 500);
