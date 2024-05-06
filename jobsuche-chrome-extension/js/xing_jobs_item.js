f6a2e00367684c183f62dd96af759cf9dd9c58a8
taras_shevch <taras.shevchuk@gmail.com>
2024-05-06 17:13:54 +0200

    parse job posting info and print to console

--- jobsuche-chrome-extension/js/xing_jobs_item.js
+++ jobsuche-chrome-extension/js/xing_jobs_item.js
@@ -11,7 +11,21 @@ function processContent() {
     console.log(contentSection.innerHTML)
 
 
-    
+    const jobTitle = document.querySelector('h1[data-testid=job-title]').textContent
+    const companyName = document.querySelector('a[data-testid=company-logo-link] p').textContent
+    const companyUrl = document.querySelector('a[data-testid=company-logo-link]').getAttribute('href')
+    const cityName = document.querySelector('li[data-testid=location-info-icon').textContent
+    console.log(jobTitle)
+    console.log(companyName)
+    console.log(companyUrl)
+    console.log(cityName)
+
+    const isHybrid = 
+        [... document.querySelectorAll('div[data-testid=static-job-details-header] span[data-xds=Marker]')]
+            .filter(it => it.textContent == 'Hybrid')
+            .length > 0
+
+    console.log('isHybrid: ' + isHybrid)
 }
 
 setTimeout(processContent, 3000)
